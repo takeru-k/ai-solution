fd08285a78dcb5b907ec5f44c59cb85560be79de
takeru-k <kam41893@gmail.com>
2023-06-23 17:03:18 +0900

    add files

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.l-header__nav, .c-hamburger__line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+});
+
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+
+   slidesPerView: 1.2,
+   breakpoints: {
+    // スライドの表示枚数：500px以上の場合
+    500: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  },
+  spaceBetween: 16,
+  
+  autoplay: { // 自動再生
+    delay: 3000, // 1.5秒後に次のスライド
+  },
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
